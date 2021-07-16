b6c3622f127136a1cedc8f79f2ea97b0176f4d0d
C0l0red <blaizepaschal@gmail.com>
2021-07-16 07:44:57 +0100

    Updaed database env key

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,7 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(__name__)
 
-    app.config["SQLALCHEMY_DATABASE_URI"] = environ.get('DATABASE_URL')
+    app.config["SQLALCHEMY_DATABASE_URI"] = environ.get('DATABASE_URI')
     db.init_app(app)
     app.config["SECRET_KEY"] = environ.get("SECRET_KEY")
 
