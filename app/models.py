e9f5025a29bb957c341e60d426540054c48a4b30
C0l0red <blaizepaschal@gmail.com>
2021-07-19 06:38:49 +0100

    Updated database

--- app/models.py
+++ app/models.py
@@ -19,7 +19,6 @@ class User(db.Model, UserMixin):
         self.email = email
         self.username = username
         self.set_password(password)
-        # super().__init__(password=password, username=kwargs["username"])
 
     def __repr__(self) -> str:
         return self.username
@@ -27,7 +26,6 @@ class User(db.Model, UserMixin):
     def set_password(self, password):
         hash_ = generate_password_hash(password, method="sha256")
         self.password = hash_
-        # return hash_
 
     def check_password(self, password):
         return check_password_hash(self.password, password)
@@ -37,8 +35,6 @@ class Subscriber(db.Model):
     name = db.Column(db.String(50))
     IMSI = db.Column(db.String(20))
     call_duration = db.Column(db.Integer)
-    # calls_sent = db.relationship("Call", backref="caller")
-    # calls_received = db.relationship("Call", backref="receiver")
 
     def __init__(self, **kwargs):
         self.IMSI = str(random.randint(100_000_000_000_000, 999_999_999_999_999))
@@ -52,16 +48,20 @@ class Subscriber(db.Model):
 class Simulation(db.Model):
     id = db.Column(db.Integer, primary_key=True, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=True)
-    # calls = db.relationship("Call", backref="simulation")
     date_created = db.Column(db.DateTime, nullable=False, default=datetime.datetime.utcnow)
+    number_of_calls = db.Column(db.Integer, nullable=False)
+    number_of_base_stations = db.Column(db.Integer, nullable=False)
+    number_of_channels = db.Column(db.Integer, nullable=False)
 
-class Call(db.Model):
-    id = db.Column(db.Integer, primary_key=True, nullable=False)
-    duration = db.Column(db.Integer, nullable=False)
-    caller_id = db.Column(db.Integer, db.ForeignKey("subscriber.id"), nullable=False)
-    receiver_id = db.Column(db.Integer, db.ForeignKey("subscriber.id"), nullable=False)
-    simualation_id = db.Column(db.Integer, db.ForeignKey("simulation.id"), nullable=False)
+    user = db.relationship("User", backref="simulations")
 
-    caller = db.relationship("Subscriber", backref="calls_sent", foreign_keys=[caller_id])
-    receiver = db.relationship("Subscriber", backref="calls_receiver", foreign_keys=[receiver_id])
-    simulation = db.relationship("Simulation", backref="calls")
+# class Call(db.Model):
+#     id = db.Column(db.Integer, primary_key=True, nullable=False)
+#     duration = db.Column(db.Integer, nullable=False)
+#     caller_id = db.Column(db.Integer, db.ForeignKey("subscriber.id"), nullable=False)
+#     receiver_id = db.Column(db.Integer, db.ForeignKey("subscriber.id"), nullable=False)
+#     simualation_id = db.Column(db.Integer, db.ForeignKey("simulation.id"), nullable=False)
+
+#     caller = db.relationship("Subscriber", backref="calls_sent", foreign_keys=[caller_id])
+#     receiver = db.relationship("Subscriber", backref="calls_receiver", foreign_keys=[receiver_id])
+#     simulation = db.relationship("Simulation", backref="calls")
